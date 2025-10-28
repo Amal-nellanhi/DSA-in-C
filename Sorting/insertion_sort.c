091820dfbee0129a493fa8d87d2aba97401ae6ce
Amal-nellanhi <amalnellanhi@gmail.com>
2025-10-28 22:53:14 +0530

    Update Sorting - Added insertion sort

---
+++ Sorting/insertion_sort.c
@@ -0,0 +1,42 @@
+/* PROGRAM TO IMPLEMENT INSERTION SORT */
+# include <stdio.h>
+# include <stdlib.h>
+void insertionSort(int n , int arr[])
+{
+    int key ,j;
+    for(int i = 1 ; i< n ; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while(j>= 0 && arr[j] > key)
+        {
+            arr[j+1] = arr[j];
+            j --;
+        }
+        arr[j+1] = key;
+    }
+}
+int main()
+{
+    int n ;
+    printf("Enter the number of elements : \n");
+    scanf("%d",&n);
+    int array[n];
+    printf("Enter the array elements : \n");
+    for(int i = 0 ; i < n ; i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    printf("The original array :\n");
+    for(int i = 0 ; i < n ; i++)
+    {
+        printf("%d   ",array[i]);
+    }
+    insertionSort(n,array);
+    printf("\n");
+    printf("The Sorted array :\n");
+    for(int i = 0 ; i < n ; i++)
+    {
+        printf("%d   ",array[i]);
+    }
+}
